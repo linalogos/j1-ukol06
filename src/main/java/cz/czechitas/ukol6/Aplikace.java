b18d9561a81d1f2f9aa93a6b5af3d9234134e7dc
Elina Logozinskaya <esav903@gmail.com>
2022-06-20 21:11:36 +0200

    Implemetace fragmentu formulare v Aplikace.java.

--- src/main/java/cz/czechitas/ukol6/Aplikace.java
+++ src/main/java/cz/czechitas/ukol6/Aplikace.java
@@ -9,6 +9,21 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 
 public class Aplikace extends JFrame {
+    private JLabel husyLabel;
+    private JLabel kraliciLabel;
+    private JLabel pocetHlavLabel;
+    private JLabel pocetNohouLabel;
+
+    private JSpinner husySpinner;
+    private JSpinner kraliciSpinner;
+
+    private SpinnerNumberModel husySpinnerNumberModel;
+    private SpinnerNumberModel kraliciSpinnerNumberModel;
+
+    private JTextField pocetHlavTextFiled;
+    private JTextField pocetNohouTextField;
+
+    private JButton vypocitatButton;
 
     public static void main(String[] args) {
         FlatLightLaf.setup();
@@ -31,9 +46,63 @@ public class Aplikace extends JFrame {
         setLayout(new MigLayout("wrap 2", "[right]rel[50:120:150,grow,fill]"));
         setMinimumSize(new Dimension(250, 200));
 
-        //TODO implementovat formulář podle zadání
+        husySpinnerNumberModel = new SpinnerNumberModel(0,0,null,1);
+
+        husySpinner = new JSpinner(husySpinnerNumberModel);
+        husyLabel = new JLabel("Husy");
+        husyLabel.setDisplayedMnemonic('h');
+        husyLabel.setLabelFor(husySpinner);
+
+        add(husyLabel);
+        add(husySpinner);
+
+        kraliciSpinnerNumberModel = new SpinnerNumberModel(0, 0, null, 1);
+
+        kraliciSpinner = new JSpinner(kraliciSpinnerNumberModel);
+        kraliciLabel = new JLabel("Kralici");
+        kraliciLabel.setDisplayedMnemonic('m');
+        kraliciLabel.setLabelFor(kraliciSpinner);
+
+        add(kraliciLabel);
+        add(kraliciSpinner);
+
+        vypocitatButton = new JButton("Vypočítat");
+        vypocitatButton.setMnemonic('v');
+
+        add(vypocitatButton, "center, span");
+
+        vypocitatButton.addActionListener(this :: handleVypocitat);
+
+        pocetHlavTextFiled = new JTextField();
+        pocetHlavTextFiled.setEnabled(false);
+        pocetHlavTextFiled.setHorizontalAlignment(JTextField.TRAILING);
+
+        pocetHlavLabel = new JLabel("Počet hlav");
+        pocetHlavLabel.setLabelFor(pocetHlavTextFiled);
+
+        add(pocetHlavTextFiled);
+        add(pocetHlavLabel);
+
+        pocetNohouTextField = new JTextField();
+        pocetNohouTextField.setEnabled(false);
+        pocetHlavTextFiled.setHorizontalAlignment(JTextField.TRAILING);
+
+        pocetNohouLabel = new JLabel("Počet nohou");
+        pocetNohouLabel.setLabelFor(pocetNohouTextField);
+
+        add(pocetNohouTextField);
+        add(pocetNohouLabel);
 
         pack();
+    }
+
+    private void handleVypocitat (ActionEvent actionEvent) {
+
+        int husy = (Integer) husySpinner.getValue();
+        int kralici = (Integer) kraliciSpinner.getValue();
+
+        pocetHlavTextFiled.setText(Integer.toString(husy + kralici));
+        pocetNohouTextField.setText(Integer.toString(husy*2 + kralici*4));
     }
 
 }
